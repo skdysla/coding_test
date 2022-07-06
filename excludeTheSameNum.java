2cfb67cece3fb066b7bf210fa4b3d5cf595c5b54
skdysla <skdysla@gmail.com>
2022-07-06 20:46:44 +0900

    modify

--- excludeTheSameNum.java
+++ excludeTheSameNum.java
@@ -12,10 +12,7 @@ public class Solution {
             }
         }
         
-        int[] answer = new int[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            answer[i] = result.get(i);
-        }
+        int[] answer = result.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }
