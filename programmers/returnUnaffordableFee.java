dd44670ad138c2c6bfd28d453a8126384ef02678
skdysla <skdysla@gmail.com>
2022-07-27 20:56:40 +0900

    commit

--- programmers/returnUnaffordableFee.java
+++ programmers/returnUnaffordableFee.java
@@ -2,9 +2,9 @@ import java.math.*;
 
 class Solution {
     public long solution(long price, long money, long count) {
-        long p = (long)price;
-        long m = (long)money;
-        long c = (long)count;
+        long p = price;
+        long m = money;
+        long c = count;
         
         for(long l = 1; l <= c; l++){
             m -= (p * l);
